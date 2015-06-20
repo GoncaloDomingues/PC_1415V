06af63ed7e7104b285d32bf38035c1b43142255e
GoncaloD <goncalomdomingues@gmail.com>
2015-06-20 15:52:06 +0100

    FutureHolder Serie1

---
+++ 2ºSérie/C#/FutureHolder_Serie1/FutureHolder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1ºSérie
+{
+   public class FutureHolder<T>
+    {
+        bool _hasValue = false;
+        T _newValue;
+        private readonly object toLock = new object();
+
+    
+        public void SetValue(T value)
+        {
+            lock (toLock)
+            {
+                _newValue = value;
+                _hasValue = true;
+                Monitor.PulseAll(toLock);
+            }
+        }
+
+        public T GetValue(long timeout)
+        {
+            lock (toLock)
+            {
+                int _lastTime = (timeout != Timeout.Infinite) ? Environment.TickCount : 0;
+
+                try
+                {
+                    Monitor.Wait(toLock);
+                }
+                catch (ThreadInterruptedException)
+                {
+                    throw new ThreadInterruptedException();
+                }
+
+                if (IsValueAvailable())
+                {
+                    Monitor.Pulse(toLock);
+                    return _newValue;
+                }
+
+                int timeFinal = (int)timeout;
+
+
+                if (SyncUtils.AdjustTimeout(ref _lastTime, ref timeFinal) == 0)
+                {
+                    Monitor.Pulse(this);
+                    return default(T);
+                }
+
+                return default(T);
+            }
+        }
+
+        public bool IsValueAvailable()
+        {
+            return _hasValue;
+        }
+    }
+}
